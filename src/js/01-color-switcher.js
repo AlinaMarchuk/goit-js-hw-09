d95093c9c9278dff6739cbdce77cec977c67034a
AlinaMarchuk <123773400+AlinaMarchuk@users.noreply.github.com>
2023-03-28 14:31:41 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStartEl: document.querySelector('button[data-start]'),
+  btnStopEl: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+refs.btnStartEl.addEventListener('click', onBtnStartElClick);
+refs.btnStopEl.addEventListener('click', onBtnStopElClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onBtnStartElClick(e) {
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStartEl.setAttribute('disabled', '');
+  refs.btnStopEl.removeAttribute('disabled');
+}
+
+function onBtnStopElClick(e) {
+  clearInterval(timerId);
+  refs.btnStopEl.setAttribute('disabled', '');
+  refs.btnStartEl.removeAttribute('disabled');
+}
